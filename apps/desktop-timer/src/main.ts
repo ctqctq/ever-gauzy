cb0bff3aac42a845152b6207e1c5124d9855336c
Adolphe Kifungo <akifungo@gmail.com>
2022-11-15 11:05:44 +0200

    feat: integrate sentry to angular

--- apps/desktop-timer/src/main.ts
+++ apps/desktop-timer/src/main.ts
@@ -5,6 +5,8 @@ import { environment } from './environments/environment';
 
 import { enableAkitaProdMode, persistState } from '@datorama/akita';
 import { akitaConfig } from '@datorama/akita';
+import * as Sentry from '@sentry/angular';
+import { BrowserTracing } from '@sentry/tracing';
 
 if (environment.production) {
 	enableProdMode();
@@ -19,6 +21,29 @@ akitaConfig({
 	resettable: true
 });
 
+Sentry.init({
+	dsn: environment.SENTRY_DSN,
+	integrations: [
+		// Registers and configures the Tracing integration,
+		// which automatically instruments your application to monitor its
+		// performance, including custom Angular routing instrumentation
+		new BrowserTracing({
+			tracingOrigins: [
+				'localhost',
+				'https://api.gauzy.co/api',
+				environment.AWHost
+			],
+			routingInstrumentation: Sentry.routingInstrumentation
+		})
+	],
+
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 1.0
+});
+
 platformBrowserDynamic()
 	.bootstrapModule(AppModule)
+	.then((success) => console.log(`Bootstrap success`))
 	.catch((err) => console.error(err));
--- apps/desktop-timer/src/sentry.ts
+++ apps/desktop-timer/src/sentry.ts
@@ -1,12 +1,13 @@
-const { init } =
-	process.type === 'browser'
-		? require('@sentry/electron/main')
-		: require('@sentry/electron/renderer');
+import { init, IPCMode } from '@sentry/electron';
 
 import { environment } from './environments/environment';
 
 export function initSentry() {
 	if (environment.SENTRY_DSN) {
-		init({ dsn: environment.SENTRY_DSN });
+		init({
+			dsn: environment.SENTRY_DSN,
+			debug: true,
+			ipcMode: IPCMode.Both
+		});
 	}
 }
